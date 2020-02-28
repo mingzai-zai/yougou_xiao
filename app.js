78bd5fe05fa9309927c743207ca08df703e8989c
mingzai-zai <mingzai_2020@163.com>
2020-02-28 15:37:06 +0800

    首页轮播图完成

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //app.js
+import axios from "utils/axios.js"
 App({
   onLaunch: function () {
+    axios.defaults.baseURL ="https://api-hmugo-web.itheima.net/api/public/v1"
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,18 @@
 //index.js
-
+import axios from "../../utils/axios.js"
 Page({
-  
+  data:{
+    swiperlist:[],
+  },
+  onLoad(){
+    axios({
+      url:'/home/swiperdata'
+    }).then(res=>{
+      // console.log(res);
+      let {message} = res.data;
+      this.setData({
+        swiperlist:message
+      })
+    })
+  }
 })
