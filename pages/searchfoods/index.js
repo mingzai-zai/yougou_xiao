8731d31e29c8dbbb64f7f3ddc5ba299c1a38ad95
mingzai-zai <mingzai_2020@163.com>
2020-03-01 10:40:19 +0800

    商品列表初步完成

--- pages/searchfoods/index.js
+++ pages/searchfoods/index.js
@@ -1,12 +1,62 @@
 // pages/searchfoods/index.js
+import axios from "../../utils/axios.js"
 Page({
   data:{
-    current:0
+    current:0,
+      query:'',
+      cid:'',
+      pagenum:1,
+      foodslist:[],
   },
   typetab(e){
     this.setData({
       current:e.currentTarget.dataset.index
     })
+  },
+  aboutfoods(e){
+    this.setData({
+        query:e.detail.value, 
+    })
+    // console.log(this.data.query)
+    axios({
+      url:"/goods/search",
+      data:this.data.query,
+    }).then(res=>{
+      let {goods} = res.data.message;
+      this.setData({
+        foodslist:goods,
+      })
+    })
+  },
+  onReachBottom(){
+    let page =1;
+    this.setData({
+      pagenum:page+this.data.pagenum,
+    })
+    // console.log(this.data.query)
+    axios({
+      url: "/goods/search",
+      data: {
+        query: this.data.query,
+        pagenum:this.data.pagenum,
+      }
+    }).then(res => {
+      // console.log(res)
+      if(res.data.message.total===0) {
+        wx.showToast({
+          title: '没哟数据了哦',
+          icon: 'success',
+          duration: 2000
+        })
+        return;
+      }
+      let { goods } = res.data.message;
+      goods.forEach(e=>{
+        this.data.foodslist.push(e)
+      })
+      this.setData({
+        foodslist:this.data.foodslist,
+      })
+    })
   }
-  
 })
