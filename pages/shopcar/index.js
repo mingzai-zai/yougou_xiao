08b0e4a05b65098d9f3d52e81280e4e0403bde90
mingzai-zai <mingzai_2020@163.com>
2020-03-06 13:51:56 +0800

    购物车的获取地址完成

--- pages/shopcar/index.js
+++ pages/shopcar/index.js
@@ -1,66 +1,28 @@
 // pages/shopcar/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    useraddress:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //一开始加载地址
+  onLoad(){
+    const userinfo = wx.getStorageSync('userinfo') || {};
+    this.setData({
+      useraddress:userinfo
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //登录获取地址
+  addaddress(){
+    wx.chooseAddress({
+      success:(res)=> {
+        this.setData({
+          useraddress:{
+            username:res.userName,
+            phone: res.telNumber,
+            address: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        wx.setStorageSync('userinfo', this.data.useraddress)
+      }
+    })
   }
 })
